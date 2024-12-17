d0972e54ff12d032ce54e0c602f70d864563a08d
Reaz Rahman <reazrahman@tempo.fios-router.home>
2024-12-17 13:02:19 -0500

    instructions updated

--- src/main/java/org/example/GUIComponents/StockEntryInstruction.java
+++ src/main/java/org/example/GUIComponents/StockEntryInstruction.java
@@ -13,11 +13,12 @@ public class StockEntryInstruction extends AbstractGUIComponent
                 "<html>" +
                         "1) Click 'Add' to enter a new stock, or 'Delete' to remove the bottom-most stock from the list.<br>" +
                         "2) Make as many edits as you'd like, but be cautious before clicking 'Save'. Saving is irreversible.<br>" +
-                        "3) From the 'View' menu, select 'Reset' to clear all stocks from your panel.<br>" +
-                        "4) From the 'View' menu, select 'Reload' to repopulate the panel with saved data." +
+                        "3) From the 'View' menu, select 'Reset' to clear all stocks from your panel <br>" +
+                        "&nbsp;&nbsp;&nbsp;&nbsp;3.1) select 'Reload' to repopulate the panel with saved data. <br>" +
+                        "4) To delete a particular stock, sets its quantity and cost to 0 and hit 'Save'" +
                         "</html>";
         JLabel label = new JLabel(instructions, JLabel.CENTER);
-        m_panel.setPreferredSize(new Dimension(100, 80));
+        m_panel.setPreferredSize(new Dimension(100, 90));
         m_panel.add(label);
     }
 }
