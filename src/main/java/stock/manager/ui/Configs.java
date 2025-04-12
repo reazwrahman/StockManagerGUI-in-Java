6c7bfd7a2506b13094c1135bd5f42297c9c160fd
Reaz Rahman <reazrahman@tempo.fios-router.home>
2025-04-12 13:27:32 -0400

    changed image

--- src/main/java/stock/manager/ui/Configs.java
+++ src/main/java/stock/manager/ui/Configs.java
@@ -2,5 +2,5 @@ package stock.manager.ui;
 
 public class Configs {
     public static final String STOCK_ENTRY_FILE_NAME = "StockEntries.json";
-    public static final String COVER_PHOTO = "cover_photo.png";
+    public static final String COVER_PHOTO = "cover_photo_2.jpg";
 }
